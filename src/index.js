5db3918d5ad8eab2848c0f3884cf086cd5605fa2
hokhai846 <hokhai846@gmail.com>
2022-07-27 18:43:38 +0700

    routing cho blog

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
+const route = require("./routes");
 
 //static files
 app.use(express.static(path.join(__dirname, "public")));
@@ -19,13 +20,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources\\views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+//Routes init
+route(app);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
